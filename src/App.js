be8b6585d8ebe2bc60a47d0e2f66f9218520b9e0
l0x539 <nogues322@gmail.com>
2023-11-30 11:03:20 +0100

    [Fix] loading.

--- src/App.js
+++ src/App.js
@@ -22,6 +22,16 @@ function App() {
 
   return (
     <>
+
+      {loadingProgress < 100 && <div
+        className={`h-full w-full flex items-center justify-center ${isTablet ? 'gap-60 text-xl': isMobile ? 'gap-28 text-sm' : 'gap-96 text-lg'} ${loadingProgress >= 100 ? 'animate-none bg-transparent':'animate-pulse bg-slate-100'}`}>
+        <div className={`${loadingProgress >= 100 ? 'hidden':'visible'}`}>
+          <svg className="animate-spin -ml-1 mr-3 h-8 w-8 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        </div>
+      </div>}
       <Canvas
         gl={{
           antialias: false,
@@ -48,16 +58,6 @@ function App() {
         {navigator.userAgent.indexOf('Mac OS X') !== -1 || GPUTier.tier < 3 ? <></>: <EffectComposer disableNormalPass >
           <N8AO aoRadius={50} distanceFalloff={0.2} intensity={3} screenSpaceRadius halfRes />
         </EffectComposer>}
-
-        <Html fullscreen
-        className={`flex items-center justify-center ${isTablet ? 'gap-60 text-xl': isMobile ? 'gap-28 text-sm' : 'gap-96 text-lg'} ${loadingProgress >= 100 ? 'animate-none bg-transparent':'animate-pulse bg-slate-100'}`}>
-          <div className={`${loadingProgress >= 100 ? 'hidden':'visible'}`}>
-            <svg className="animate-spin -ml-1 mr-3 h-8 w-8 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
-          </div>
-        </Html>
       </Canvas>
     </>
   );
